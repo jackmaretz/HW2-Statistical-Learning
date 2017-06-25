b268d9a0d72430cf186e398dc9af3d1ee64474ea
jackmaretz <giacomomaretto@gmail.com>
2017-06-26 01:51:11 +0200

    Adding files and scripts

---
+++ import.R
@@ -0,0 +1,28 @@
+library(wrassp) 
+#library(help = wrassp)
+library(tuneR)
+#Read the .au file 
+
+# read.AsspDataObj ------------------------------------------------------------------------
+
+temp = list.files(pattern ="*.au")
+
+create_empty_table <- function(num_rows, num_cols) {
+  frame <- data.frame(matrix(NA, nrow = num_rows, ncol = num_cols))
+  return(frame)
+}
+
+
+for (i in 1:length(temp)) {
+  x = read.AsspDataObj(temp[i])
+  y = Wave(as.numeric(x$audio), samp.rate = rate.AsspDataObj(x), bit = 16)
+  assign(temp[i], y)
+  }
+
+# Feature extraction ------------------------------------------------------
+
+suppressPackageStartupMessages( require(signal, quietly = TRUE) )
+
+# setup of short time fourier transform -----------------------------------
+
+fs = 
